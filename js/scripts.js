1329521144082090e363368a0933c9eb08f7f09b
Austin Schrader <austin.jschrader@gmail.com>
2020-10-21 10:42:09 -0700

    Upload Template

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,1 @@
-$(document).ready(function () {
-	$('#formOne').submit(function (event) {
-		const firstNameInput = $('input#first-name').val();
-		const middleNameInput = $('input#middle-name').val();
-		const lastNameInput = $('input#last-name').val();
-
-		$('.first-name-output').text(firstNameInput);
-		$('.middle-name-output').text(middleNameInput);
-		$('.last-name-output').text(lastNameInput);
-
-		$('#letter').show();
-
-		event.preventDefault();
-	});
-});
+$(document).ready(function () {});
