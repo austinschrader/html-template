cfd974f9cd2eea88b7db8a6744fc6c69e0a123a6
Austin Schrader <austin.jschrader@gmail.com>
2020-10-21 10:30:55 -0700

    Add .val() to correct the input field JS variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	$('#formOne').submit(function (event) {
-		const firstNameInput = $('input#first-name');
-		const middleNameInput = $('input#middle-name');
-		const lastNameInput = $('input#last-name');
+		const firstNameInput = $('input#first-name').val();
+		const middleNameInput = $('input#middle-name').val();
+		const lastNameInput = $('input#last-name').val();
 
 		$('.first-name-output').text(firstNameInput);
 		$('.middle-name-output').text(middleNameInput);
